9cd12b1199a96f577c02dd8fc59134abbcbc4d86
IFU <tntcorporation@mail.ru>
2022-05-18 04:16:22 +0500

    fixed bugs b

--- main/comparison_lab.py
+++ main/comparison_lab.py
@@ -14,16 +14,16 @@ TYPES = (
     "шип", "лиоф", "капли", "душ", "крем",
     "паст", "мазь", "инсулин",
     "рект", "гранулы", "инфузия", "жидкий",
-    "увл", "sprey", "комплект", 'аэр',
+    "увл", "sprey", 'аэр'
 
     )
 
 EXTRA_TYPES = (
-   "ультра",  "супер", "плюс", "мини", "д/детей", "детский", 
-    "plus", "super", "baby", "беби"
+   "ультра", 'ultra',  "супер", "плюс", "мини", "д/детей", "детский", 
+    "plus", "super", "baby", "беби", "комплект"
 )
 
-MEASURES = ("гр", "мг", "г", "мл", "МЕ","мкг", "ЕД", "%", "XXL", "M", "S", "2XL", "XL")
+MEASURES = ("гр", "мг", "г", "мл", "МЕ","мкг", "ед", "%", "xxl", "M", "S", "2xl", "xl")
 
 
 def make_comparison(data):
@@ -56,69 +56,68 @@ def make_comparison(data):
     #COMPARISON LABARATORY!
     for i in ws_1.iter_rows():
             if i[med_col_1].value != None: #medicine cell value
+                _value_1 = str(i[med_col_1].value).lower()
+                _com_1 = str(i[com_col_1].value).lower()
                 for j in ws_2.iter_rows():          #to iter second col
+                    if j[med_col_2].value != None:
+                        _value_2 = str(j[med_col_2].value).lower()
+                        _com_2 = str(j[com_col_2].value).lower()
                         if med_col_2 < len(j):
-                            if j[med_col_2].value != None:
-                                
-                                _value_1 = str(i[med_col_1].value).lower()
-                                _value_2 = str(j[med_col_2].value).lower()
-                                
+
                                 if _value_1.isascii() and _value_2.isascii(): #to check value is latin
-                                    continue
+                                    pass
                                 elif _value_1.isascii():
                                     _value_1 = to_cyrillic(_value_1)
                                 elif _value_2.isascii():
                                     _value_2 = to_cyrillic(_value_2)
-                                    
                                 
-                                if  _value_1[0:6] == _value_2[0:6]:
-                                    for typ3 in TYPES:
-                                        if typ3 in _value_1 and typ3 in _value_2:
-                                            
-                                            for measure in MEASURES:
-                                                if measure in _value_1 and measure in _value_2:
-                                                    if regex_of_measure(measure, _value_1) != None and regex_of_measure(measure, _value_2) != None:
-                                                        if digit_regex(regex_of_measure(measure, _value_1).group(0)).group(0) == digit_regex(regex_of_measure(measure, _value_2).group(0)).group(0):
-                                                            if _value_1 != None and _value_2 != None:
-                                                                if fuzz.ratio(to_latin(_value_1), to_latin(_value_2)) >= 60:
-                                                                    # for ex_type in EXTRA_TYPES:
-                                                                    #     if ex_type i[med_col_1].value.lower()
-                                                                    # print(i[med_col_1].value, "<=>", j[med_col_2].value)
-                                                                    NEW_FILE_VAlUES += ((i,j),)
-                                                                    break
-                                                            
-                                                            # print(i[med_col_1].value,"<->", j[med_col_2].value, "__1__")
-                                                            NEW_FILE_VAlUES += ((i,j),)
-                                                            break
-                                            break
-                                        elif typ3 in _value_1 or typ3 in _value_2:
-                                            print("ASdasd")
-                                            continue
-                                        else:
-                                            for measure in MEASURES:
-                                                print(_value_1, _value_2, "mes")
-                                                if measure in _value_1 and measure in _value_2:
-                                                    if regex_of_measure(measure, _value_1) != None and regex_of_measure(measure, _value_2) != None:
-                                                        if digit_regex(regex_of_measure(measure, _value_1).group(0)).group(0) == digit_regex(regex_of_measure(measure, _value_2).group(0)).group(0):
-                                                            if i[com_col_1].value != None and j[com_col_2].value != None:
-                                                                if fuzz.ratio(to_latin(i[com_col_1].value.lower()), to_latin(j[com_col_2].value.lower())) >= 65:
-                                                                    # print(i[com_col_1].value, "<*=*>", j[com_col_2].value, i[med_col_1].value, "<->", j[med_col_2].value)
-                                                                    NEW_FILE_VAlUES += ((i,j),)
-                                                                    break
-                                                            
-                                                            # print(i[com_col_1].value, "<*=*>", j[com_col_2].value, i[med_col_1].value, "<->", j[med_col_2].value)
-                                                            NEW_FILE_VAlUES += ((i,j),)        
-                                                            break
-                                                elif measure in _value_1 and measure not in _value_2 or measure not in _value_1 and measure in _value_2:
-                                                    continue
-                                                else:
-                                                    if fuzz.ratio(_value_1, _value_2) >= 85:
-                                                        if i[com_col_1].value != None and j[com_col_2].value != None:
-                                                            if fuzz.ratio(to_latin(i[com_col_1].value.lower()), to_latin(j[com_col_2].value.lower())) >= 65:
-                                                                        # print(i[com_col_1].value, "<*=*>", j[com_col_2].value, i[med_col_1].value, "<->", j[med_col_2].value)
+                                if _com_1.isascii() and _com_2.isascii():
+                                    pass
+                                elif not _com_1.isascii():
+                                    _com_1 = to_latin(_com_1)
+                                elif not _com_2.isascii():
+                                    _com_2 = to_latin(_com_2)
+                                
+                                if  _value_1[0:7] == _value_2[0:7]:
+                                    if fuzz.ratio(_com_1, _com_2) >= 65:
+                                        for typ3 in TYPES:
+                                            if typ3 in _value_1 and typ3 in _value_2:
+                                                for measure in MEASURES:
+                                                    if measure in _value_1 and measure in _value_2:
+                                                        
+                                                        for extra_type in EXTRA_TYPES:
+                                                            if extra_type in _value_1 and extra_type in _value_2:
+                                                                if set(digit_regex(_value_1)) == set(digit_regex(_value_2)):          
                                                                         NEW_FILE_VAlUES += ((i,j),)
                                                                         break
-                                            break
+                                                        if not any(extra_type in _value_1 for extra_type in EXTRA_TYPES) and not any(extra_type in _value_2 for extra_type in EXTRA_TYPES):
+                                                            if set(digit_regex(_value_1)) == set(digit_regex(_value_2)):          
+                                                                NEW_FILE_VAlUES += ((i,j),)
+                                                                break
+                                                        break
+                                                if not any(m3asure in _value_1 for m3asure in MEASURES) and not any(m3asure in _value_2 for m3asure in MEASURES):
+                                                    for extra_type in EXTRA_TYPES:
+                                                        if extra_type in _value_1 and extra_type in _value_2:
+                                                            if set(digit_regex(_value_1)) == set(digit_regex(_value_2)):          
+                                                                    NEW_FILE_VAlUES += ((i,j),)
+                                                                    break
+                                                    if not any(extra_type in _value_1 for extra_type in EXTRA_TYPES) and not any(extra_type in _value_2 for extra_type in EXTRA_TYPES):
+                                                        if set(digit_regex(_value_1)) == set(digit_regex(_value_2)):          
+                                                            NEW_FILE_VAlUES += ((i,j),)
+                                                            break
+                                                    break
+
+                                        if not any(t1p3 in _value_1 for t1p3 in TYPES) and not any(t1p3 in _value_2 for t1p3 in TYPES):
+                                            if fuzz.ratio(_value_1, _value_2) >= 80:
+                                                for extra_type in EXTRA_TYPES:
+                                                    if extra_type in _value_1 and extra_type in _value_2:
+                                                        if set(digit_regex(_value_1)) == set(digit_regex(_value_2)):          
+                                                            NEW_FILE_VAlUES += ((i,j),)
+                                                            break
+                                                if not any(extra_type in _value_1 for extra_type in EXTRA_TYPES) and not any(extra_type in _value_2 for extra_type in EXTRA_TYPES):
+                                                    if set(digit_regex(_value_1)) == set(digit_regex(_value_2)):          
+                                                        NEW_FILE_VAlUES += ((i,j),)
+
 
     wb.close()
     create_excel(NEW_FILE_VAlUES)
@@ -126,15 +125,18 @@ def make_comparison(data):
 def create_excel(values):
     wb = Workbook()
     sheet = wb.active
-    sheet.title = "RESULT 5 star"
+    sheet.title = "RESULT"
     for index, data in enumerate(values):
         cnt_col = 1
         for zero_index in  range(len(data[0])):
-            sheet.cell(row=index+1,  column=cnt_col).value=data[0][zero_index].value
-            cnt_col += 1
+
+            if data[0][zero_index].value != None:
+                sheet.cell(row=index+1,  column=cnt_col).value=data[0][zero_index].value
+                cnt_col += 1
         for first_index in range(len(data[1])):
-            sheet.cell(row=index+1, column=cnt_col).value=data[1][first_index].value
-            cnt_col += 1
+            if data[1][first_index].value != None:
+                sheet.cell(row=index+1, column=cnt_col).value=data[1][first_index].value
+                cnt_col += 1
 
     
     wb.save(MEDIA_ROOT / "sample.xlsx")
--- main/regexes.py
+++ main/regexes.py
@@ -1,8 +1,9 @@
-from re import search
+from re import findall
 
-def regex_of_measure(text, value):
+def regex_of_measure(value):
     """"RETURN regex of value check with text!"""
-    return search(f"\d+\{text}\/\d+[A-zА-я]|\d+\{text}|\d+\s+\{text}|\d+\.*\d+\{text}|\d+\.\d+\s+\{text}|\d+\,\d+\{text}|\d+\,\d+\s+\{text}", value)
+    # return search(f"\d+\{text}\/*\d+[A-zА-я]|\d+\.*\d+\{text}|\d+\s+\{text}|\d+\{text}|\d+\.\d+\s+\{text}|\d+\,\d+\{text}|\d+\,\d+\s+\{text}", value)
+    return findall(f"\d+[A-zА-я]|\d+%|\№*\s+\d+|\№*\d+|\d+\s+[A-zА-я]|\d+\.*\d+[A-zА-я]|\d+\.\d+\s+[A-zА-я]|\d+\,\d+[A-zА-я]|\d+\,\d+\s+[A-zА-я]|\d+[A-zА-я]", value)
 
 def digit_regex(text):
-    return search("\d+\,*\d+|\d+\.*\d+|\d+", text)
+    return findall("\d+\,*\d+|\d+\.*\d+|\d+", text)
