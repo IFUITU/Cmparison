2ab870d726cbf3087f45b7b2714d5e79eb8f3299
ifuitu <ifuitu@gmail.com>
2023-02-25 21:18:43 +0500

    ....

--- client/models.py
+++ client/models.py
@@ -6,3 +6,13 @@ class User(AbstractUser):
     REQUIRED_FIELDS = ['phone']
     phone = models.CharField(max_length=13, unique=True, null=True)#, validators=[PhoneValidator()])
     company_name = models.CharField(max_length=256, null=True)
+
+    def __init__(self, *args, **kwargs):
+        super(User, self).__init__(*args, **kwargs)
+        self.old_password = self.password
+
+
+    def save(self, *args, **kwargs):
+        if self._state.adding or (self.password != self.old_password):
+            super().set_password(self.password)
+        super().save(*args, **kwargs)
